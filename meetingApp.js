b041a9c1571d7a93100af79a829ae096935d5383
Luis Rangel <xlarsx@gmail.com>
2016-03-18 15:38:16 +0100

    Extracted route for handling meetings

--- meetingApp.js
+++ meetingApp.js
@@ -1,40 +1,10 @@
-require('babel-polyfill');
-
 const express = require('express');
 const debug = require('debug')('ma:index');
 
-const router = express.Router();
-
-router.use((req, res, next) => {
-  debug(`/${req.method}`);
-  next();
-});
-
-router.get('/user/:id', (req, res, next) => {
-  debug(req.params.id);
-  if (req.params.id) {
-    res.json({
-      message: 'You must pass ID other than 0',
-    });
-  } else {
-    next();
-  }
-});
-
-router.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World',
-  });
-});
-
-router.get('/user/:id', (req, res) => {
-  res.json({
-    message: `Hello ${req.parms.id}`,
-  });
-});
+const scheduleMeeting = require('./routes/scheduleMeeting');
 
 const meetingApp = express();
-meetingApp.use('/', router);
+meetingApp.use('/meeting', scheduleMeeting);
 meetingApp.listen(3000, () => {
   debug('Live at Port 3000');
 });
