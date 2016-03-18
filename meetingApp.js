5d45776e30f08ca65ca6ff7eb136f50f199ac521
Luis Rangel <xlarsx@gmail.com>
2016-03-18 11:31:13 +0100

    Airbnb implemented

--- meetingApp.js
+++ meetingApp.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require('express').Router;
 const meetingApp = express();
-const debug = require('debug')('ma:index')
+const debug = require('debug')('ma:index');
 
-const router = express.Router();
+const router = express();
 
 router.use((req, res, next) => {
   debug(`/${req.method}`);
@@ -10,31 +10,32 @@ router.use((req, res, next) => {
 });
 
 router.get('/user/:id', (req, res, next) => {
-  debug(req.params.id)
+  debug(req.params.id);
   if (req.params.id) {
     res.json({
-      message: 'You must pass ID other than 0'
+      message: 'You must pass ID other than 0',
     });
+  } else {
+    next();
   }
-  else next();
 });
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
   res.json({
-    message : 'Hello World'
+    message: 'Hello World',
   });
 });
 
-router.get('/user/:id', (req, res, next) => {
+router.get('/user/:id', (req, res) => {
   res.json({
-    message: `Hello ${req.parms.id}`
+    message: `Hello ${req.parms.id}`,
   });
 });
 
-meetingApp.use('/',router);
+meetingApp.use('/', router);
 
 // Listen to this Port
 
 meetingApp.listen(3000, () => {
-  console.log('Live at Port 3000');
+  debug('Live at Port 3000');
 });
