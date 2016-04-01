dff9d1c5b5dfc6d1af3817b9512d56f95374da4c
Luis Rangel <xlarsx@gmail.com>
2016-04-01 13:31:21 +0200

    Extracted the port as part of the configuration file

--- meetingApp.js
+++ meetingApp.js
@@ -1,16 +1,22 @@
 require("babel-polyfill");
 require('es6-promise').polyfill();
 
+const nconf = require('nconf');
 const express = require('express');
 const debug = require('debug')('ma:index');
 const bodyParser = require('body-parser');
 
 const scheduleMeeting = require('./routes/scheduleMeeting');
 
+nconf.argv().env();
+nconf.defaults({ PORT: 3000 });
+
+const port = nconf.get("PORT");
+
 const meetingApp = express();
 meetingApp.use(bodyParser.json());
 meetingApp.use(bodyParser.urlencoded());
 meetingApp.use('/meeting', scheduleMeeting);
-meetingApp.listen(3000, () => {
-  debug('Live at Port 3000');
+meetingApp.listen(port, () => {
+  debug(`Live at Port ${port}`);
 });
