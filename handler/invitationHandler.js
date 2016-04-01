d7cb2498d0e54bfa3a6ff7542df01acc5629d139
Luis Rangel <xlarsx@gmail.com>
2016-04-01 16:22:51 +0200

    Removed dependencies

--- handler/invitationHandler.js
+++ handler/invitationHandler.js
@@ -1,13 +1,10 @@
 const resolveToString = require('es6-template-strings/resolve-to-string');
 const compile = require('es6-template-strings/compile');
-const Firebase = require('firebase');
 const debug = require('debug')('ma:handler:invitationHandler');
 const fs = require('fs');
 const path = require('path');
-const moment = require('moment');
 const mailHandler = require('./mailHandler');
 const meetingHandler = require('./meetingHandler');
-const nconf = require('nconf');
 
 const filePath = path.join(__dirname, '../public/emailToGuest.html');
 const templateText = fs.readFileSync(filePath, 'utf-8');
