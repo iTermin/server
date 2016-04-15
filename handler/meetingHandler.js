ee7b33175d8d72ec8e4b8cfbc996b6cae46e0229
Luis Rangel <xlarsx@gmail.com>
2016-04-15 13:28:39 +0200

    Changes promises, to use async await

--- handler/meetingHandler.js
+++ handler/meetingHandler.js
@@ -5,46 +5,41 @@ const nconf = require('nconf');
 
 const dateFormat = 'DD-MM-YYYY HH:mm:ss ZZ';
 
-function getMeetingDetail(meetingId) {
-  // TODO: Improve the usage of the promises
-  return new Promise((resolve, reject) => {
-    const firebaseRef = new Firebase(nconf.get('FIREBASE_PATH'));
-    firebaseRef.child(`Meetings/${meetingId}`).once('value', (dataSnapshot) => {
-      const meetingInformation = dataSnapshot.val();
-      if (meetingInformation) {
-        debug('Extracted information from meetingId', meetingId);
+async function getMeetingDetail(meetingId) {
+ const firebaseRef = new Firebase(nconf.get('FIREBASE_PATH'));
+ const dataSnapshot = await firebaseRef.child(`Meetings/${meetingId}`).once('value');
+ const meetingInformation = dataSnapshot.val();
+ if(!meetingInformation) throw new Error('Invalid meeting');
+ if (meetingInformation) {
+  debug('Extracted information from meetingId', meetingId);
 
-        const hostName = getMeetingHostName(meetingInformation);
-        const subject = `${hostName}'s invitation`;
-        const guests = getMeetingGuests(meetingInformation);
+  const hostName = getMeetingHostName(meetingInformation);
+  const subject = `${hostName}'s invitation`;
+  const guests = getMeetingGuests(meetingInformation);
 
-        // TODO: Use this information instead of the guets (#21)
-        const startDate = moment(meetingInformation.detail.startDate, dateFormat);
-        const endDate = moment(meetingInformation.detail.endDate, dateFormat);
-        const dateMeeting = startDate.format('LLLL');
-        const nameMeeting = meetingInformation.detail.name;
-        const durationMeeting = startDate.from(endDate, true);
+  // TODO: Use this information instead of the guets (#21)
+  const startDate = moment(meetingInformation.detail.startDate, dateFormat);
+  const endDate = moment(meetingInformation.detail.endDate, dateFormat);
+  const dateMeeting = startDate.format('LLLL');
+  const nameMeeting = meetingInformation.detail.name;
+  const durationMeeting = startDate.from(endDate, true);
 
-        const formattedDetails = {
-          startDate,
-          endDate,
-          dateMeeting,
-          nameMeeting,
-          durationMeeting,
-        };
+  const formattedDetails = {
+   startDate,
+   endDate,
+   dateMeeting,
+   nameMeeting,
+   durationMeeting,
+  };
 
-        for (let guestIndex = 1; guestIndex < guests.length; ++guestIndex) {
-          const guest = guests[guestIndex];
-          guest.meetingDetail = getGuestMeetingInformation(meetingInformation, guest, hostName);
-        }
-        resolve({ meetingInformation, hostName, subject, guests, formattedDetails });
-      } else {
-        reject('Invalid meeting');
-      }
-    }, (err) => {
-      reject(err);
-    });
-  });
+  for (let guestIndex = 1; guestIndex < guests.length; ++guestIndex) {
+   const guest = guests[guestIndex];
+   guest.meetingDetail = getGuestMeetingInformation(meetingInformation, guest, hostName);
+  }
+  return { meetingInformation, hostName, subject, guests, formattedDetails };
+ } else {
+  reject('Invalid meeting');
+ }
 }
 
 function updateStatusForGuest(meetingId, index, status) {
