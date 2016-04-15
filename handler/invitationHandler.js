e10fbfea5d6c83a2b5f87f6393076646a2d32651
Luis Rangel <xlarsx@gmail.com>
2016-04-15 14:56:22 +0200

    Cleaning libraries

--- handler/invitationHandler.js
+++ handler/invitationHandler.js
@@ -1,12 +1,10 @@
-const resolveToString = require('es6-template-strings/resolve-to-string');
-const compile = require('es6-template-strings/compile');
 const debug = require('debug')('ma:handler:invitationHandler');
-const fs = require('fs');
 const path = require('path');
 const nconf = require('nconf');
+const ejs = require('ejs');
+
 const mailHandler = require('./mailHandler');
 const meetingHandler = require('./meetingHandler');
-const ejs = require('ejs');
 
 const filePath = path.join(__dirname, '../public/emailToGuest.html');
 
@@ -20,7 +18,13 @@ async function sendInvitationFromMeeting(meetingId) {
 
     const baseURI = nconf.get('BASE_URI');
     const meetingURL = `${baseURI}/meetingDetail/${meetingId}/${guestIndex}`;
-    ejs.renderFile(filePath, { ...meetingGeneralInfo, ...guest.meetingDetail, baseURI, meetingURL }, (error, html) => {
+    ejs.renderFile(filePath, {
+      ...meetingGeneralInfo,
+      ...guest.meetingDetail,
+      baseURI, meetingURL,
+    }, (error, html) => {
+      if(error) throw error;
+
       mailHandler(guest.email, subject, html);
     });
   }
