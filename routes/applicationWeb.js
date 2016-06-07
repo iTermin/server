c20c6892b83dbc678256316cf2a13504ac645784
Estefania Chavez Guardado <fachinacg@gmail.com>
2016-06-07 22:09:09 +0200

    All request to the app is handled by the client

--- routes/applicationWeb.js
+++ routes/applicationWeb.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const route = express.Router();
 
-route.get('/', async (req, res, next) => {
+route.get('/*', async (req, res, next) => {
   try {
     res.render('webApplication');
   } catch (err) {
