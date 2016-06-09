984d8df6957f0a474f24369dadb1e032094113f0
Estefania Chavez Guardado <fachinacg@gmail.com>
2016-06-09 18:52:07 +0200

    Including current components

--- public/js/webApplication/app.js
+++ public/js/webApplication/app.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import Footer from './components/Footer'
-import AddMeeting from './containers/AddMeeting'
-import VisibleMeetingsList from './containers/VisibleMeetingsList'
-
+import React from 'react';
+import Footer from './components/Footer';
+import AddMeeting from './containers/AddMeeting';
+import VisibleMeetingsList from './containers/VisibleMeetingsList';
+import reducers from './reducers';
 import ReactDOM from "react-dom";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+let store = createStore(reducers);
 const App = () => (
+        <Provider store={store}>
         <div>
-        Hola!
+        <AddMeeting />
+        <VisibleMeetingsList />
+        <Footer />
         </div>
+        </Provider>
 )
 
-ReactDOM.render(
-        <App />,
-    document.body
-);
+ReactDOM.render(<App />, document.body);
